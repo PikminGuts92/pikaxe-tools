423b9341d1d15fed1eb0ea495fcdba4348a45eca
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-07-05 21:44:49 -0400

    Initial commit

---
+++ apps/character_creator/src/main.rs
@@ -0,0 +1,10 @@
+// Hide console if release build
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .run();
+}
